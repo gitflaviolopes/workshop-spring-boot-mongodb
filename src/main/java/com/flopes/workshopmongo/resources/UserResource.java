d66cdc9a56c04ca8fb876d3637f715f85c69080d
Flavio Lopes <flavio.euzebio@gmail.com>
2021-06-03 14:31:37 -0300

    Obtendo um usuário por id

--- src/main/java/com/flopes/workshopmongo/resources/UserResource.java
+++ src/main/java/com/flopes/workshopmongo/resources/UserResource.java
@@ -1,13 +1,13 @@
 package com.flopes.workshopmongo.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flopes.workshopmongo.domain.User;
@@ -21,11 +21,18 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method=RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
