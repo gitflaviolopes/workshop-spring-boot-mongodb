b6c4930ee889fb3f6d8240fe07607493300e14f1
Flavio Lopes <flavio.euzebio@gmail.com>
2021-06-03 14:39:52 -0300

    Inserção de usuário com POST

--- src/main/java/com/flopes/workshopmongo/services/UserService.java
+++ src/main/java/com/flopes/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.flopes.workshopmongo.domain.User;
+import com.flopes.workshopmongo.dto.UserDTO;
 import com.flopes.workshopmongo.repository.UserRepository;
 import com.flopes.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,13 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDto(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }
